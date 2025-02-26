273c04673c6af326df72a440bde732df50ca56e3
RSbyarc <Doberman_ru@inbox.ru>
2025-02-27 02:59:19 +0600

    Update js.js

--- js.js
+++ js.js
@@ -1,14 +1,1 @@
-let lastScrollTop = 0; // Переменная для отслеживания предыдущей прокрутки
 
-window.addEventListener('scroll', function() {
-    let header = document.querySelector('header');
-    let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
-
-    // Если прокручиваем вниз и позиция больше предыдущей
-    if (currentScroll > lastScrollTop && currentScroll > 100) {
-        header.classList.add('shrunk'); // Скрываем шапку
-    } else {
-        header.classList.remove('shrunk'); // Показываем шапку
-    }
-    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // Обновляем предыдущую прокрутку
-});
