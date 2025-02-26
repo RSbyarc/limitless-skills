2054f7d033bff9ca95ed4669ce8dac8d281a5c84
RSbyarc <Doberman_ru@inbox.ru>
2025-02-27 02:45:27 +0600

    Update js.js

--- js.js
+++ js.js
@@ -1,16 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
     let lastScrollTop = 0;
     const header = document.querySelector('header');
-
+    
     window.addEventListener('scroll', function() {
         let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
 
         if (currentScroll > lastScrollTop) {
-            // Когда прокручиваем вниз, скрыть шапку
-            header.style.top = "-80px"; 
+            // Когда прокручиваем вниз, скрыть шапку и сделать её компактной
+            header.style.top = "-80px";
+            header.classList.add('shrunk'); // Добавляем класс для сжатия
         } else {
-            // Когда прокручиваем вверх, показать шапку
-            header.style.top = "0"; 
+            // Когда прокручиваем вверх, вернуть шапку и убрать сжатие
+            header.style.top = "0";
+            header.classList.remove('shrunk');
         }
         lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
     });
