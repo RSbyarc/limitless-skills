7e07a1f54469ae52f464ffe4b8d79ea47fbe55bb
RSbyarc <Doberman_ru@inbox.ru>
2025-02-27 02:54:04 +0600

    Update js.js

--- js.js
+++ js.js
@@ -1,15 +1,14 @@
-// JavaScript для скрытия шапки при прокрутке
-let lastScrollTop = 0; // Переменная для отслеживания предыдущего положения
+let lastScrollTop = 0; // Переменная для отслеживания предыдущей прокрутки
 
 window.addEventListener('scroll', function() {
     let header = document.querySelector('header');
     let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
 
-    // Если прокручено вниз и текущая позиция больше предыдущей, скрыть шапку
+    // Если прокручиваем вниз и позиция больше предыдущей
     if (currentScroll > lastScrollTop && currentScroll > 100) {
-        header.classList.add('shrunk'); // Добавляем класс для скрытия шапки
+        header.classList.add('shrunk'); // Скрываем шапку
     } else {
-        header.classList.remove('shrunk'); // Убираем класс, когда прокрутка вверх
+        header.classList.remove('shrunk'); // Показываем шапку
     }
-    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // Чтобы не получить отрицательные значения
+    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // Обновляем предыдущую прокрутку
 });
